47b49fc2d3deee29b9e036ae1602a5571d41d5b0
Ryan Kim <kyhekm@hotmail.com>
2015-06-30 12:31:59 -0700

    Code updated for uploading photos on Parse

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 import Parse
 
 class TimelineViewController: UIViewController {
-
+    
     var photoTakingHelper: PhotoTakingHelper?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.delegate = self
         // Do any additional setup after loading the view.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -34,22 +34,21 @@ class TimelineViewController: UIViewController {
             let post = PFObject(className: "Post")
             post["imageFile"] = imageFile
             post.save()
-            
         }
-
-        }
+        
+    }
     
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
 
 //MARK: Tab Bar Delegate
